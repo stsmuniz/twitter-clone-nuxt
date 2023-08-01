ccaa394ed9b7a009f2d1365c09cbdc10a0853918
Salustiano Muniz <stsmuniz@gmail.com>
2023-08-01 06:51:11 +0000

    Update 4 files

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -1,89 +1,108 @@
+import jwt_decode from "jwt-decode";
+
 export default () => {
-    const useAuthToken = () => useState('authToken')
-    const useAuthUser = () => useState('authUser')
-    const useAuthLoading = () => useState('authLoading', () => true)
+  const useAuthToken = () => useState("authToken");
+  const useAuthUser = () => useState("authUser");
+  const useAuthLoading = () => useState("authLoading", () => true);
 
-    const setToken = (newToken) => {
-        const authToken = useAuthToken()
-        authToken.value = newToken
+  const setToken = (newToken) => {
+    const authToken = useAuthToken();
+    authToken.value = newToken;
+  };
+
+  const setUser = (newUser) => {
+    const authUser = useAuthUser();
+    authUser.value = newUser;
+  };
+
+  const setIsLoading = (value) => {
+    const authLoading = useAuthLoading();
+    authLoading.value = value;
+  };
+
+  const login = ({ username, password }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await $fetch("/api/auth/login", {
+          method: "POST",
+          body: {
+            username,
+            password,
+          },
+        });
+
+        setToken(data.accessToken);
+        setUser(data.user);
+
+        resolve(true);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const refreshToken = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await $fetch("/api/auth/refresh");
+        setToken(data.accessToken);
+        resolve(true);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const getUser = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await useFetchApi("/api/auth/user");
+        setUser(data.user);
+        resolve(true);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const reRefreshAccessToken = () => {
+    const authToken = useAuthToken();
+
+    if (!authToken) {
+      return;
     }
 
-    const setUser = (newUser) => {
-        const authUser = useAuthUser()
-        authUser.value = newUser
-    }
+    const jwt = jwt_decode(authToken.value);
 
-    const setIsLoading = (value) => {
-        const authLoading = useAuthLoading()
-        authLoading.value = value
-    }
+    const newRefreshTime = jwt.exp - 6000;
 
-    const login = ({ username, password }) => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const data = await $fetch('/api/auth/login', {
-                    method: 'POST',
-                    body: {
-                        username,
-                        password
-                    }
-                })
+    setTimeout(async () => {
+      await refreshToken();
+      reRefreshAccessToken();
+    }, newRefreshTime);
+  };
 
-                setToken(data.accessToken)
-                setUser(data.user)
+  const initAuth = () => {
+    return new Promise(async (resolve, reject) => {
+      setIsLoading(true);
+      try {
+        await refreshToken();
+        await getUser();
+        reRefreshAccessToken();
+        resolve(true);
+      } catch (error) {
+        reject(error);
+      } finally {
+        setIsLoading(false);
+      }
+    });
+  };
 
-                resolve(true)
-            } catch (error) {
-                reject(error)
-            }
-        })
-    }
-
-    const refreshToken = () => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const data = await $fetch('/api/auth/refresh')
-                setToken(data.accessToken)
-                resolve(true)
-            } catch (error) {
-                reject(error)
-            }
-        })
-    }
-
-    const getUser = () => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const data = await useFetchApi('/api/auth/user')
-                setUser(data.user)
-                resolve(true)
-            } catch (error) {
-                reject(error)
-            }
-        })
-    }
-
-    const initAuth = () => {
-        return new Promise(async (resolve, reject) => {
-            setIsLoading(true)
-            try {
-                await refreshToken()
-                await getUser()
-
-                resolve(true)
-            } catch (error) {
-                reject(error)
-            } finally {
-                setIsLoading(false)
-            }
-        })
-    }
-
-    return {
-        login,
-        useAuthUser,
-        useAuthToken,
-        useAuthLoading,
-        initAuth
-    }
-}
+  return {
+    login,
+    useAuthUser,
+    useAuthToken,
+    useAuthLoading,
+    initAuth,
+  };
+};
